5d8858a73752879afdd6a2b52a54cd3b0140f1f3
Dmitry Atapin <73831539+AtapinDmitry@users.noreply.github.com>
2024-09-09 14:03:30 +0300

    Integration tests migrated to testSuite usage (#31)
    * Integration tests migrated to testSuite usage
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * Foundation version modified
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * linter settings updated
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * go.mod fixed
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * Integration tests migrated to testSuite usage
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * Foundation version modified
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * linter settings updated
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * go.mod fixed
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * Integration tests migrated to testSuite usage
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * Foundation version modified
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * go.mod fixed
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    ---------
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- test/integration/swap/swap_test.go
+++ test/integration/swap/swap_test.go
@@ -2,26 +2,13 @@ package swap
 
 import (
 	"encoding/json"
-	"os"
-	"path/filepath"
-	"syscall"
-	"time"
-
 	"github.com/anoideaopen/foundation/core/types"
 	pbfound "github.com/anoideaopen/foundation/proto"
 	"github.com/anoideaopen/foundation/test/integration/cmn"
 	"github.com/anoideaopen/foundation/test/integration/cmn/client"
-	"github.com/anoideaopen/foundation/test/integration/cmn/fabricnetwork"
-	"github.com/anoideaopen/foundation/test/integration/cmn/runner"
-	docker "github.com/fsouza/go-dockerclient"
-	"github.com/hyperledger/fabric/integration/nwo"
-	"github.com/hyperledger/fabric/integration/nwo/fabricconfig"
-	runnerFbk "github.com/hyperledger/fabric/integration/nwo/runner"
+	"github.com/hyperledger/fabric/integration"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
-	"github.com/tedsuo/ifrit"
-	ginkgomon "github.com/tedsuo/ifrit/ginkgomon_v2"
 )
 
 const (
@@ -32,7 +19,6 @@ const (
 	fnBalanceOf        = "balanceOf"
 	fnAllowedBalanceOf = "allowedBalanceOf"
 	fnSwapBegin        = "swapBegin"
-	fnSwapGet          = "swapGet"
 	fnSwapDone         = "swapDone"
 
 	emitAmount      = "1000"
@@ -43,194 +29,69 @@ const (
 
 var _ = Describe("Robot swap tests", func() {
 	var (
-		testDir          string
-		cli              *docker.Client
-		network          *nwo.Network
-		networkProcess   ifrit.Process
-		ordererProcesses []ifrit.Process
-		peerProcesses    ifrit.Process
+		ts client.TestSuite
 	)
 
 	BeforeEach(func() {
-		networkProcess = nil
-		ordererProcesses = nil
-		peerProcesses = nil
-		var err error
-		testDir, err = os.MkdirTemp("", "foundation")
-		Expect(err).NotTo(HaveOccurred())
-
-		cli, err = docker.NewClientFromEnv()
-		Expect(err).NotTo(HaveOccurred())
+		ts = client.NewTestSuite(components)
 	})
-
 	AfterEach(func() {
-		if networkProcess != nil {
-			networkProcess.Signal(syscall.SIGTERM)
-			Eventually(networkProcess.Wait(), network.EventuallyTimeout).Should(Receive())
-		}
-		if peerProcesses != nil {
-			peerProcesses.Signal(syscall.SIGTERM)
-			Eventually(peerProcesses.Wait(), network.EventuallyTimeout).Should(Receive())
-		}
-		if network != nil {
-			network.Cleanup()
-		}
-		for _, ordererInstance := range ordererProcesses {
-			ordererInstance.Signal(syscall.SIGTERM)
-			Eventually(ordererInstance.Wait(), network.EventuallyTimeout).Should(Receive())
-		}
-		err := os.RemoveAll(testDir)
-		Expect(err).NotTo(HaveOccurred())
+		ts.ShutdownNetwork()
 	})
 
 	var (
-		channels         = []string{cmn.ChannelAcl, cmn.ChannelCC, cmn.ChannelFiat}
-		ordererRunners   []*ginkgomon.Runner
-		redisProcess     ifrit.Process
-		redisDB          *runner.RedisDB
-		networkFound     *cmn.NetworkFoundation
-		robotProc        ifrit.Process
-		skiBackend       string
-		skiRobot         string
-		peer             *nwo.Peer
-		admin            *client.UserFoundation
-		user             *client.UserFoundation
-		feeSetter        *client.UserFoundation
-		feeAddressSetter *client.UserFoundation
+		channels = []string{cmn.ChannelAcl, cmn.ChannelCC, cmn.ChannelFiat}
+		user     *client.UserFoundation
 	)
 	BeforeEach(func() {
 		By("start redis")
-		redisDB = &runner.RedisDB{}
-		redisProcess = ifrit.Invoke(redisDB)
-		Eventually(redisProcess.Ready(), runnerFbk.DefaultStartTimeout).Should(BeClosed())
-		Consistently(redisProcess.Wait()).ShouldNot(Receive())
+		ts.StartRedis()
 	})
 	BeforeEach(func() {
-		networkConfig := nwo.MultiNodeSmartBFT()
-		networkConfig.Channels = nil
-
-		pchs := make([]*nwo.PeerChannel, 0, cap(channels))
-		for _, ch := range channels {
-			pchs = append(pchs, &nwo.PeerChannel{
-				Name:   ch,
-				Anchor: true,
-			})
-		}
-		for _, peer := range networkConfig.Peers {
-			peer.Channels = pchs
-		}
-
-		network = nwo.New(networkConfig, testDir, cli, StartPort(), components)
-		cwd, err := os.Getwd()
-		Expect(err).NotTo(HaveOccurred())
-		network.ExternalBuilders = append(network.ExternalBuilders,
-			fabricconfig.ExternalBuilder{
-				Path:                 filepath.Join(cwd, ".", "externalbuilders", "binary"),
-				Name:                 "binary",
-				PropagateEnvironment: []string{"GOPROXY"},
-			},
-		)
-
-		networkFound = cmn.New(network, channels)
-		networkFound.Robot.RedisAddresses = []string{redisDB.Address()}
-
-		networkFound.GenerateConfigTree()
-		networkFound.Bootstrap()
-
-		for _, orderer := range network.Orderers {
-			runner := network.OrdererRunner(orderer)
-			runner.Command.Env = append(runner.Command.Env, "FABRIC_LOGGING_SPEC=orderer.consensus.smartbft=debug:grpc=debug")
-			ordererRunners = append(ordererRunners, runner)
-			proc := ifrit.Invoke(runner)
-			ordererProcesses = append(ordererProcesses, proc)
-			Eventually(proc.Ready(), network.EventuallyTimeout).Should(BeClosed())
-		}
-
-		peerGroupRunner, _ := fabricnetwork.PeerGroupRunners(network)
-		peerProcesses = ifrit.Invoke(peerGroupRunner)
-		Eventually(peerProcesses.Ready(), network.EventuallyTimeout).Should(BeClosed())
-
-		By("Joining orderers to channels")
-		for _, channel := range channels {
-			fabricnetwork.JoinChannel(network, channel)
-		}
-
-		By("Waiting for followers to see the leader")
-		Eventually(ordererRunners[1].Err(), network.EventuallyTimeout, time.Second).Should(gbytes.Say("Message from 1"))
-		Eventually(ordererRunners[2].Err(), network.EventuallyTimeout, time.Second).Should(gbytes.Say("Message from 1"))
-		Eventually(ordererRunners[3].Err(), network.EventuallyTimeout, time.Second).Should(gbytes.Say("Message from 1"))
-
-		By("Joining peers to channels")
-		for _, channel := range channels {
-			network.JoinChannel(channel, network.Orderers[0], network.PeersWithChannel(channel)...)
-		}
-
-		peer = network.Peer("Org1", "peer0")
-
-		pathToPrivateKeyBackend := network.PeerUserKey(peer, "User1")
-		skiBackend, err = cmn.ReadSKI(pathToPrivateKeyBackend)
-		Expect(err).NotTo(HaveOccurred())
-
-		pathToPrivateKeyRobot := network.PeerUserKey(peer, "User2")
-		skiRobot, err = cmn.ReadSKI(pathToPrivateKeyRobot)
-		Expect(err).NotTo(HaveOccurred())
-
-		admin, err = client.NewUserFoundation(pbfound.KeyType_ed25519)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(admin.PrivateKeyBytes).NotTo(Equal(nil))
-
-		feeSetter, err = client.NewUserFoundation(pbfound.KeyType_ed25519)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(feeSetter.PrivateKeyBytes).NotTo(Equal(nil))
-
-		feeAddressSetter, err = client.NewUserFoundation(pbfound.KeyType_ed25519)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(feeAddressSetter.PrivateKeyBytes).NotTo(Equal(nil))
-
-		cmn.DeployACL(network, components, peer, testDir, skiBackend, admin.PublicKeyBase58, admin.KeyType)
-		cmn.DeployCC(network, components, peer, testDir, skiRobot, admin.AddressBase58Check)
-		cmn.DeployFiat(network, components, peer, testDir, skiRobot,
-			admin.AddressBase58Check, feeSetter.AddressBase58Check, feeAddressSetter.AddressBase58Check)
+		ts.InitNetwork(channels, integration.GatewayBasePort)
+		ts.DeployChaincodes()
 	})
 	BeforeEach(func() {
 		By("start robot")
-		robotRunner := networkFound.RobotRunner()
-		robotProc = ifrit.Invoke(robotRunner)
-		Eventually(robotProc.Ready(), network.EventuallyTimeout).Should(BeClosed())
+		ts.StartRobot()
 	})
 	AfterEach(func() {
 		By("stop robot")
-		if robotProc != nil {
-			robotProc.Signal(syscall.SIGTERM)
-			Eventually(robotProc.Wait(), network.EventuallyTimeout).Should(Receive())
-		}
-		By("stop redis " + redisDB.Address())
-		if redisProcess != nil {
-			redisProcess.Signal(syscall.SIGTERM)
-			Eventually(redisProcess.Wait(), time.Minute).Should(Receive())
-		}
+		ts.StopRobot()
+		By("stop redis")
+		ts.StopRedis()
 	})
 
 	BeforeEach(func() {
 		By("add admin to acl")
-		client.AddUser(network, peer, network.Orderers[0], admin)
+		ts.AddAdminToACL()
 
 		By("add user to acl")
 		var err error
 		user, err = client.NewUserFoundation(pbfound.KeyType_ed25519)
 		Expect(err).NotTo(HaveOccurred())
 
-		client.AddUser(network, peer, network.Orderers[0], user)
+		ts.AddUser(user)
 
 		By("emit tokens")
-		client.TxInvokeWithSign(network, peer, network.Orderers[0],
-			cmn.ChannelFiat, cmn.ChannelFiat, admin,
-			fnEmit, "", client.NewNonceByTime().Get(), nil, user.AddressBase58Check, emitAmount)
+		ts.TxInvokeWithSign(
+			cmn.ChannelFiat,
+			cmn.ChannelFiat,
+			ts.Admin(),
+			fnEmit,
+			"",
+			client.NewNonceByTime().Get(),
+			user.AddressBase58Check,
+			emitAmount,
+		).CheckErrorIsNil()
 
 		By("emit check")
-		client.Query(network, peer, cmn.ChannelFiat, cmn.ChannelFiat,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(emitAmount), nil),
-			fnBalanceOf, user.AddressBase58Check)
+		ts.Query(
+			cmn.ChannelFiat,
+			cmn.ChannelFiat,
+			fnBalanceOf,
+			user.AddressBase58Check,
+		).CheckBalance(emitAmount)
 	})
 
 	It("swap test", func() {
@@ -241,48 +102,42 @@ var _ = Describe("Robot swap tests", func() {
 
 		By("swap from fiat to cc")
 		By("swap begin")
-		swapBeginTxID := client.TxInvokeWithSign(network, peer, network.Orderers[0],
-			cmn.ChannelFiat, cmn.ChannelFiat, user,
-			fnSwapBegin, "", client.NewNonceByTime().Get(), nil,
-			ccFiatUpper, ccCCUpper, emitAmount, defaultSwapHash)
+		swapBeginTxID := ts.TxInvokeWithSign(
+			cmn.ChannelFiat,
+			cmn.ChannelFiat,
+			user,
+			fnSwapBegin,
+			"",
+			client.NewNonceByTime().Get(),
+			ccFiatUpper,
+			ccCCUpper,
+			emitAmount,
+			defaultSwapHash,
+		).TxID()
 		Expect(swapBeginTxID).ToNot(BeEmpty())
 
 		By("swap get")
-		fGet := func(out []byte) string {
-			if len(out) == 0 {
-				return "out is empty"
-			}
-
-			return ""
-		}
-		client.Query(network, peer, cmn.ChannelCC, cmn.ChannelCC,
-			fabricnetwork.CheckResult(fGet, nil),
-			fnSwapGet, swapBeginTxID)
+		ts.SwapGet(cmn.ChannelCC, cmn.ChannelCC, client.SfnSwapGet, swapBeginTxID)
 
 		By("check balance 1")
-		client.Query(network, peer, cmn.ChannelFiat, cmn.ChannelFiat,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(zeroAmount), nil),
-			fnBalanceOf, user.AddressBase58Check)
+		ts.Query(cmn.ChannelFiat, cmn.ChannelFiat, fnBalanceOf, user.AddressBase58Check).
+			CheckBalance(zeroAmount)
 
 		By("check allowed balance 1")
-		client.Query(network, peer, cmn.ChannelCC, cmn.ChannelCC,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(zeroAmount), nil),
-			fnAllowedBalanceOf, user.AddressBase58Check, ccFiatUpper)
+		ts.Query(cmn.ChannelCC, cmn.ChannelCC, fnAllowedBalanceOf, user.AddressBase58Check, ccFiatUpper).
+			CheckBalance(zeroAmount)
 
 		By("swap done")
-		client.NBTxInvoke(network, peer, network.Orderers[0], nil,
-			cmn.ChannelCC, cmn.ChannelCC,
-			fnSwapDone, swapBeginTxID, defaultSwapKey)
+		ts.NBTxInvoke(cmn.ChannelCC, cmn.ChannelCC, fnSwapDone, swapBeginTxID, defaultSwapKey).
+			CheckErrorIsNil()
 
 		By("check balance 2")
-		client.Query(network, peer, cmn.ChannelFiat, cmn.ChannelFiat,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(zeroAmount), nil),
-			fnBalanceOf, user.AddressBase58Check)
+		ts.Query(cmn.ChannelFiat, cmn.ChannelFiat, fnBalanceOf, user.AddressBase58Check).
+			CheckBalance(zeroAmount)
 
 		By("check allowed balance 2")
-		client.Query(network, peer, cmn.ChannelCC, cmn.ChannelCC,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(emitAmount), nil),
-			fnAllowedBalanceOf, user.AddressBase58Check, ccFiatUpper)
+		ts.Query(cmn.ChannelCC, cmn.ChannelCC, fnAllowedBalanceOf, user.AddressBase58Check, ccFiatUpper).
+			CheckBalance(emitAmount)
 	})
 
 	It("Multiswap test", func() {
@@ -296,48 +151,41 @@ var _ = Describe("Robot swap tests", func() {
 			},
 		})
 		Expect(err).NotTo(HaveOccurred())
-		swapBeginTxID := client.TxInvokeWithSign(network, peer, network.Orderers[0],
-			cmn.ChannelFiat, cmn.ChannelFiat, user,
-			"multiSwapBegin", "", client.NewNonceByTime().Get(), nil,
-			"FIAT", string(assets), "CC", defaultSwapHash)
+		swapBeginTxID := ts.TxInvokeWithSign(
+			cmn.ChannelFiat,
+			cmn.ChannelFiat,
+			user,
+			"multiSwapBegin",
+			"",
+			client.NewNonceByTime().Get(),
+			"FIAT",
+			string(assets),
+			"CC",
+			defaultSwapHash,
+		).TxID()
 		Expect(swapBeginTxID).ToNot(BeEmpty())
 
 		By("multiswap get 1")
-		fGet := func(out []byte) string {
-			if len(out) == 0 {
-				return "out is empty"
-			}
-
-			return ""
-		}
-		client.Query(network, peer, cmn.ChannelCC, cmn.ChannelCC,
-			fabricnetwork.CheckResult(fGet, nil),
-			"multiSwapGet", swapBeginTxID)
+		ts.SwapGet(cmn.ChannelCC, cmn.ChannelCC, client.SfnMultiSwapGet, swapBeginTxID)
 
 		By("check balance 1")
-		client.Query(network, peer, cmn.ChannelFiat, cmn.ChannelFiat,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(zeroAmount), nil),
-			"balanceOf", user.AddressBase58Check)
+		ts.Query(cmn.ChannelFiat, cmn.ChannelFiat, fnBalanceOf, user.AddressBase58Check).
+			CheckBalance(zeroAmount)
 
 		By("check allowed balance 1")
-		client.Query(network, peer, cmn.ChannelCC, cmn.ChannelCC,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(zeroAmount), nil),
-			"allowedBalanceOf", user.AddressBase58Check, ccFiatUpper)
+		ts.Query(cmn.ChannelCC, cmn.ChannelCC, fnAllowedBalanceOf, user.AddressBase58Check, ccFiatUpper).
+			CheckBalance(zeroAmount)
 
 		By("multiswap done")
-		client.NBTxInvoke(network, peer, network.Orderers[0], nil,
-			cmn.ChannelCC, cmn.ChannelCC,
-			"multiSwapDone", swapBeginTxID, defaultSwapKey)
+		ts.NBTxInvoke(cmn.ChannelCC, cmn.ChannelCC, "multiSwapDone", swapBeginTxID, defaultSwapKey).
+			CheckErrorIsNil()
 
 		By("check balance 2")
-		client.Query(network, peer, cmn.ChannelFiat, cmn.ChannelFiat,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(zeroAmount), nil),
-			"balanceOf", user.AddressBase58Check)
+		ts.Query(cmn.ChannelFiat, cmn.ChannelFiat, fnBalanceOf, user.AddressBase58Check).
+			CheckBalance(zeroAmount)
 
 		By("check allowed balance 2")
-		client.Query(network, peer, cmn.ChannelCC, cmn.ChannelCC,
-			fabricnetwork.CheckResult(fabricnetwork.CheckBalance(emitAmount), nil),
-			"allowedBalanceOf", user.AddressBase58Check, ccFiatUpper)
-
+		ts.Query(cmn.ChannelCC, cmn.ChannelCC, fnAllowedBalanceOf, user.AddressBase58Check, ccFiatUpper).
+			CheckBalance(emitAmount)
 	})
 })
